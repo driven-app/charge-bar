368269647fb93711acab664d360385f35076f17f
Damien Glancy <damien.glancy@icloud.com>
2021-02-09 21:23:23 +0000

    Initial checkin

---
+++ ChargeBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ChargeBar
+//
+//  Created by Damien Glancy on 09/02/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# charge-bar
+# Charge Bar
+
+A small macOS menubar app to show current state of charge for your Porsche.
