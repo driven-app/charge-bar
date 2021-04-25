34d4d8ec8184929e1db7b6bb3470626474d46960
Damien Glancy <damien.glancy@icloud.com>
2021-04-25 20:20:43 +0100

    Adding initial UI tests

---
+++ ChargeBarUITests/BaseUITestCase.swift
@@ -0,0 +1,53 @@
+//
+//  BaseUITestCase.swift
+//  ChargeBarUITests
+//
+//  Created by Damien Glancy on 25/04/2021.
+//
+
+import XCTest
+
+class BaseUITestCase: XCTestCase {
+
+  // MARK: - Properties
+  
+  var app = XCUIApplication()
+  var menuBarsQuery: XCUIElementQuery?
+  var menuBarItem: XCUIElement?
+  
+  // MARK: - Common lifecycle
+  
+  override func setUp() {
+    super.setUp()
+    continueAfterFailure = false
+    menuBarsQuery = app.menuBars
+    menuBarItem = menuBarsQuery!.children(matching: .statusItem).element
+    app.launchEnvironment = ["TEST_MODE": "true"]
+    app.launch()
+  }
+  
+  override func tearDown() {
+    menuBarsQuery = nil
+    menuBarItem = nil
+  }
+  
+  // MARK: - Common functions
+  
+  func tapMenuBar() {
+    menuBarItem!.click()
+  }
+
+}
+
+// MARK: - Extensions
+
+extension XCTestCase {
+  
+  // MARK: - Functions
+  
+  func wait(forElement element: XCUIElement, timeout: TimeInterval) {
+    let predicate = NSPredicate(format: "exists == 1")
+    expectation(for: predicate, evaluatedWith: element)
+    waitForExpectations(timeout: timeout)
+  }
+}
